8ca76c650843778dc50bb0041a623ef05ba1adb7
Nora Selenyi <noraselenyi@gmail.com>
2019-06-17 11:35:18 +0200

    b4

--- infinite.js
+++ infinite.js
@@ -8,12 +8,9 @@ function cheer(baselanguage){
     console.log(baselanguage);
 };
 
-function count(name, month){
-    console.log(`${name}'s age is 26 and was born in ${month} month.`);
-};
 
 function future () {
-    console.log('Rabbit')
+    console.log('HÜLYESÉG')
 };
 
 
